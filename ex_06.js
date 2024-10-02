21adc23af01f1ea971446ff2e28048258343af4b
karell606 <karell.azabe@epitech.digital>
2024-10-02 13:49:18 +0200

    ex_06.js

---
+++ ex_06.js
@@ -0,0 +1,14 @@
+function findAddresses (searchString) {
+    const addresses = [
+			 	["45 avenue des tulipes", "92130 Issy-les-Moulineaux"], ["95 avenue parmentier", "75011 Paris"], ["24 Rue Pasteur", "92400 Courbevoie"], ["Champ de Mars, 5 Av. Anatole France", "75007 Paris"], ["Pl. Louis-Armand", "75571 Paris"]];
+
+addresses.forEach(addresses => {
+	const fullAddresses = addresses.join(" ");
+		if (fullAddresses.includes(searchString)) {
+            console.log(fullAddresses);
+        }
+    });
+}
+findAddresses("tulipes");
+
+
