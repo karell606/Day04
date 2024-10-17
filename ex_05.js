2a6ebd64267a980269ac2dcf48939bb843abb8f7
karell606 <karell.azabe@epitech.digital>
2024-10-17 11:32:11 +0200

    Update ex_05.js

--- ex_05.js
+++ ex_05.js
@@ -1,13 +1,12 @@
 function getHousesPrototype() { 
 	const HousesPrototype = new Map ([
-			["Apartement 75008", 90],
-			["Apartement 94300", 105],
-			["Apartement 92300", 140],
-			["Apartement 75015", 65]
-    ]) 
+		["Apartement 75008", 90],
+		["Apartement 94300", 105],
+		["Apartement 92300", 140],
+		["Apartement 75015", 65]
+    	]); 
     
     return HousesPrototype
- 
 }
  
 console.log(getHousesPrototype()) ;
