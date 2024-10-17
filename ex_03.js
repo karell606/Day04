93f296d6e2e4a1a84254c0c88cae5ff0862bc309
karell606 <karell.azabe@epitech.digital>
2024-10-17 11:30:04 +0200

    Update ex_03.js

--- ex_03.js
+++ ex_03.js
@@ -1,13 +1,12 @@
-function fillBus (peopleAtBusStops,busSeats) {
-    let busCapacity = 0;
-
-	for (let i = 0; i < peopleAtBusStops.length; i++) {
-        busCapacity += peopleAtBusStops[i];
-		if (busCapacity > busSeats) {
-				return i;
+function fillBus(peopleAtBusStops, busSeats) {
+    let peopleInTheBus = 0;
+    for (let i = 0; i < peopleAtBusStops.length; i++) {
+        peopleInTheBus += peopleAtBusStops[i];
+        if (peopleInTheBus >= busSeats) {
+            return i + 1;
         }
     }
-
-    return -1; 
+    return -1;
 }
-
+ 
+console.log(fillBus([1, 3, 10, 1], 12));
