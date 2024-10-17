61062285430fa64999bcbf77e0c59b82fcadcf09
karell606 <karell.azabe@epitech.digital>
2024-10-17 11:14:59 +0200

    Update ex_01.js

--- ex_01.js
+++ ex_01.js
@@ -1,8 +1,8 @@
 function getAngryDog(numberOfWoofs)
 {
   let result = '';
-  for (let i = 0; i < numberOfWoofs; i++){
-    result += 'woof';
+    for (let i = 0; i < numberOfWoofs; i++) {
+      result += 'woof';
   }
   return result;
 }
